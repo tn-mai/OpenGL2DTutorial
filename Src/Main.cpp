c17b19c3626704c40fb96e9ef67841a96614afa2
tn_mai <rabitfiver@gmail.com>
2017-10-31 00:48:45 +0900

    ゲームパッドを実装.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -30,6 +30,24 @@ int main()
 
   // ÉÅÉCÉìÉãÅ[Év.
   while (!window.ShouldClose()) {
+    window.UpdateGamePad();
+    const GamePad& gamepad = window.GetGamePad();
+    glm::vec3 vec;
+    if (gamepad.buttons & GamePad::DPAD_LEFT) {
+      vec.x = -1;
+    } else if (gamepad.buttons & GamePad::DPAD_RIGHT) {
+      vec.x = 1;
+    }
+    if (gamepad.buttons & GamePad::DPAD_UP) {
+      vec.y = 1;
+    } else if (gamepad.buttons & GamePad::DPAD_DOWN) {
+      vec.y = -1;
+    }
+    if (vec.x || vec.y) {
+      vec = glm::normalize(vec);
+      sprite.Position(sprite.Position() + vec);
+    }
+
     glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
