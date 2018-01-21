ffc7a46881abf4d7ab9fa18fabd9ee1bfa27bb61
tn_mai <rabitfiver@gmail.com>
2018-01-21 21:28:29 +0900

    簡単なシーン制御機能を実装.

---
+++ Src/Scene.cpp
@@ -0,0 +1,75 @@
+/**
+* @file Scene.cpp
+*/
+#include "Scene.h"
+
+namespace Scene {
+
+/**
+* ゲームを開始する.
+*
+* @param start 開始するシーン.
+*/
+bool Manager::Start(const ScenePtr& start)
+{
+  currentScene = start;
+  nextScene = nullptr;
+  if (!currentScene->Initialize(*this)) {
+    currentScene = nullptr;
+    return false;
+  }
+  return true;
+}
+
+/**
+* シーンを更新する.
+*
+* @param dt 前回の更新からの経過時間.
+*/
+bool Manager::Update(float dt)
+{
+  if (!currentScene) {
+    return false;
+  }
+  if (isEnd) {
+    currentScene->Finalize(*this);
+    currentScene = nullptr;
+    return false;
+  }
+  currentScene->Update(*this, dt);
+  if (!nextScene) {
+    return true;
+  }
+  currentScene->Finalize(*this);
+  currentScene = nullptr;
+  if (!nextScene->Initialize(*this)) {
+    nextScene = nullptr;
+    return false;
+  }
+  currentScene = nextScene;
+  nextScene = nullptr;
+  return true;
+}
+
+/**
+* 次に実行するシーンを指定する.
+*
+* @param next 次に実行するシーン.
+*/
+void Manager::NextScene(const ScenePtr& next)
+{
+  nextScene = next;
+}
+
+/**
+* ゲームを終了する.
+*
+* 次回のUpdate()でシーンのFinalizeが呼ばれ、Update()がfalseを返すことでゲームを終了させる.
+*/
+void Manager::End()
+{
+  nextScene = nullptr;
+  isEnd = true;
+}
+
+} // namespace Scene
---
+++ Src/Scene.h
@@ -0,0 +1,50 @@
+/**
+* @file Scene.h
+*
+*/
+#ifndef SCENE_H_INCLUDED
+#define SCENE_H_INCLUDED
+#include <memory>
+#include "Node.h"
+
+namespace Scene {
+
+class Manager;
+
+/**
+* シーンの基底クラス.
+*/
+class Scene
+{
+public:
+  virtual bool Initialize(Manager& manager) = 0;
+  virtual bool Update(Manager& manager, float dt) = 0;
+  virtual void Finalize(Manager& manager) = 0;
+};
+using ScenePtr = std::shared_ptr<Scene>;
+
+/**
+* シーン管理クラス.
+*/
+class Manager
+{
+public:
+  bool Start(const ScenePtr& start);
+  bool Update(float dt);
+  void NextScene(const ScenePtr& next);
+  void End();
+  void AddChild(Node* child) { rootNode.AddChild(child); }
+  void RemoveChild(Node* child) { rootNode.RemoveChild(child); }
+  Node& RootNode() { return rootNode; }
+  const Node& RootNode() const { return rootNode; }
+
+private:
+  ScenePtr currentScene;
+  ScenePtr nextScene;
+  Node rootNode;
+  bool isEnd = false;
+};
+
+} // namespace Scene
+
+#endif // SCENE_H_INCLUDED
