895c318c66d12f62c3c5d80cbd84b4affb84d258
tn_mai <rabitfiver@gmail.com>
2018-01-30 20:06:01 +0900

    自機の弾の最大表示を決めてreserveしておく.

--- Src/Character/Player.cpp
+++ Src/Character/Player.cpp
@@ -23,6 +23,7 @@ Player::Player(const TexturePtr& tex) : Character(tex, glm::vec3(-300, 0, 0), { 
 {
   Rectangle({ glm::vec2(0 ,0), glm::vec2(64, 32) });
   Name("player");
+  shotList.reserve(256);
 }
 
 /**
@@ -30,6 +31,9 @@ Player::Player(const TexturePtr& tex) : Character(tex, glm::vec3(-300, 0, 0), { 
 */
 void Player::Shot(glm::f32 rot, glm::f32 vel, int atk)
 {
+  if (shotList.size() == shotList.capacity()) {
+    return;
+  }
   auto shot = Character::Create(Texture(), Position(), { {-32, 8 },{ 32, -8 }  }, 1);
   const auto m = glm::rotate(glm::mat4(), glm::radians(rot), glm::vec3(0, 0, 1));
   glm::vec3 velocity = glm::vec4(vel, 0, 0, 1) * m;
